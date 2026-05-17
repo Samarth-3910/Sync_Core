6360e0af7540eeb5a696eaaef8fe5aab5a806720
Samarth <151175065+Samarth-3910@users.noreply.github.com>
2026-05-17 08:50:42 +0530

    Change MongoDB connection string to placeholder
    Updated MongoDB connection string to use a placeholder URI.

--- node1/index.js
+++ node1/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const node = new RaftNode(NODE_ID, PORT, PEERS);
 
-mongoose.connect("mongodb://samarthkeshari8_db_user:s5sghJu9hvfXXZn2@ac-5ntpqe4-shard-00-00.jayytfd.mongodb.net:27017,ac-5ntpqe4-shard-00-01.jayytfd.mongodb.net:27017,ac-5ntpqe4-shard-00-02.jayytfd.mongodb.net:27017/?ssl=true&replicaSet=atlas-glt3kv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("URI");
 const StateModel = mongoose.model("State", new mongoose.Schema({
     key: String, value: String, term: Number, committedBy: Number
 }));
